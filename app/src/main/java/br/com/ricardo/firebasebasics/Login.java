1f760624f8125b4f8023a58d721f3e2f6256e473
rickale <ti.ricardo.sousa@gmail.com>
2019-03-28 22:04:19 -0300

    Criado opção de login com a conta do Google.

--- app/src/main/java/br/com/ricardo/firebasebasics/Login.java
+++ app/src/main/java/br/com/ricardo/firebasebasics/Login.java
@@ -2,6 +2,7 @@ package br.com.ricardo.firebasebasics;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,21 +11,33 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     private EditText editLoginEmail;
     private EditText editLoginSenha;
     private Button buttonLoginLogar;
     private Button buttonLoginNovoUsuario;
+    private SignInButton buttonGoogle;
     private TextView textLoginEsqueciSenha;
 
     private FirebaseAuth auth;
+    private GoogleApiClient googleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +48,12 @@ public class Login extends AppCompatActivity {
         editLoginSenha = (EditText) findViewById(R.id.edit_login_senha);
         buttonLoginLogar = (Button) findViewById(R.id.button_login_registrar);
         buttonLoginNovoUsuario = (Button) findViewById(R.id.button_login_voltar);
+        buttonGoogle = (SignInButton) findViewById(R.id.button_google);
         textLoginEsqueciSenha = (TextView) findViewById(R.id.text_login_esqueci_senha);
+
+        auth = FirebaseAuth.getInstance();
+        criaConexaoGoogle();
+
 
         buttonLoginLogar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,7 +63,6 @@ public class Login extends AppCompatActivity {
                 String senha = editLoginSenha.getText().toString().trim();
 
                 login(email, senha);
-
             }
         });
 
@@ -56,6 +73,16 @@ public class Login extends AppCompatActivity {
 
                 Intent intent = new Intent(Login.this, Cadastro.class);
                 startActivity(intent);
+            }
+        });
+
+        buttonGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Fazendo um intent pra chamar a janelinha do google com as contas de email.
+                Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                startActivityForResult(intent, 1);
 
             }
         });
@@ -66,7 +93,6 @@ public class Login extends AppCompatActivity {
 
                 Intent intent = new Intent(Login.this, ResetarSenha.class);
                 startActivity(intent);
-
             }
         });
 
@@ -92,10 +118,74 @@ public class Login extends AppCompatActivity {
 
     }
 
+    public void criaConexaoGoogle(){
+
+        //Cria a conexão com SignIn do Google
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        //Montando a "telinha" com os usuários.
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //se o resquestCode for o mesmo, recebe o retorno trazendo a janelinha.
+        if(requestCode == 1){
+
+            //Associando ao atributo resultado, o retorno do ActivityForResults.
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+
+            if(result.isSuccess()){
+                //Se o resultado for sucesso, cria a conta vinculada.
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseLogin(account);
+            }
+        }
+
+    }
+
+    private void firebaseLogin(GoogleSignInAccount account) {
+
+        //Criando uma credencial com os dados capturados lá da conexão da janelinha.
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+
+        //Manda a credencial pra Api do Google se logar com ela.
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+
+                            Intent intent = new Intent(Login.this, Perfil.class);
+                            startActivity(intent);
+
+                        } else {
+                            Toast.makeText(Login.this, "Falha na conexão.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
     @Override
     protected void onStart() {
         super.onStart();
 
         auth = Conexao.getFirebaseAuth();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, "Falha na conexão.", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/br/com/ricardo/firebasebasics/Perfil.java
+++ app/src/main/java/br/com/ricardo/firebasebasics/Perfil.java
@@ -1,45 +1,127 @@
 package br.com.ricardo.firebasebasics;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class Perfil extends AppCompatActivity {
+public class Perfil extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
 
+    private ImageView image;
     private TextView textPerfilEmail;
     private TextView textPerfilId;
     private Button buttonPerfilDeslogar;
 
     private FirebaseAuth auth;
+    private FirebaseAuth.AuthStateListener authStatelistener;
     private FirebaseUser user;
+    private GoogleApiClient googleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
 
+        image = (ImageView) findViewById(R.id.imageView);
         textPerfilEmail = (TextView) findViewById(R.id.text_resetar_email);
         textPerfilId = (TextView) findViewById(R.id.text_perfil_id);
         buttonPerfilDeslogar = (Button) findViewById(R.id.button_resetar);
+
+        inicializaFirebase();
+        criaConexaoGoogle();
 
         buttonPerfilDeslogar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                //Desconectando o Firebase.
                 Conexao.logOut();
-                finish();
+
+                //Desconectando o auth.
+                Auth.GoogleSignInApi.signOut(googleApiClient).setResultCallback(new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(@NonNull Status status) {
+                        Toast.makeText(Perfil.this, "Conta desconectada", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                });
+
             }
         });
+    }
+
+    public void criaConexaoGoogle(){
+
+        //Cria a conexão com SignIn do Google
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        //Montando a "telinha" com os usuários.
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+    }
+
+    public void inicializaFirebase(){
+
+        //Inicializei a conexão com o Firebase.
+        auth = FirebaseAuth.getInstance();
+
+        //Criei uma instancia do listener.
+        authStatelistener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                //Atributo do tipo FirebaseUser recebendo o usuário.
+                user = firebaseAuth.getCurrentUser();
+
+                if(user != null){
+
+                    //Carregando os dados nos componentes.
+                    textPerfilEmail.setText(user.getEmail());
+                    textPerfilId.setText(user.getUid());
+                    Glide.with(Perfil.this).load(user.getPhotoUrl()).into(image);
+
+                } else{
+                    finish();
+                }
+            }
+        };
+    }
+
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+        //Método override apenas implementado do Listener pra falha.
+        Toast.makeText(this, "Falha na conexão.", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+
+        //Quando houver alguma alteração, ao startar a activity, é chamado o listener pra carregar os componentes.
+        auth.addAuthStateListener(authStatelistener);
 
         //Quando a activity startar, é recuperado através dos métodos getters a autenticação e o usuário.
         auth = Conexao.getFirebaseAuth();
@@ -51,5 +133,13 @@ public class Perfil extends AppCompatActivity {
             textPerfilEmail.setText("Email: " + user.getEmail());
             textPerfilId.setText("ID: " + user.getUid());
         }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        //Retira as informações pra não sobrecarregar o aplictivo ao fechar a activity.
+        auth.removeAuthStateListener(authStatelistener);
     }
 }
