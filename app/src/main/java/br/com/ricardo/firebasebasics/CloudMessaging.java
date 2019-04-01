04e3b49d31bb780a8680d0a685cf2cab56b6c9b6
rickale <ti.ricardo.sousa@gmail.com>
2019-04-01 15:29:30 -0300

    Instalado projeto no Firebase e dependências no Gradle para o correto funcionamento do Crashlytics.

--- app/src/main/java/br/com/ricardo/firebasebasics/CloudMessaging.java
+++ app/src/main/java/br/com/ricardo/firebasebasics/CloudMessaging.java
@@ -1,19 +1,29 @@
 package br.com.ricardo.firebasebasics;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.crashlytics.android.Crashlytics;
 
 public class CloudMessaging extends AppCompatActivity {
 
+
     private TextView textCloudMessaging;
+    private Button buttonErro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cloud_messaging);
 
+
+
         textCloudMessaging = (TextView) findViewById(R.id.textNotification);
+        buttonErro = (Button) findViewById(R.id.button_erro);
 
 
         if(getIntent().getExtras() != null){
@@ -24,6 +34,19 @@ public class CloudMessaging extends AppCompatActivity {
                 textCloudMessaging.setText(v);
             }
         }
+
+        buttonErro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(CloudMessaging.this, MyFirebaseMessagingService.class);
+                startActivity(intent);
+
+                Crashlytics.getInstance().crash();
+
+
+            }
+        });
 
 
     }
