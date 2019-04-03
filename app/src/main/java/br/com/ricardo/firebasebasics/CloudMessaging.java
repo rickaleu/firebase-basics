447d008169dafe8d97f8e9717f102b8f57d1762b
rickale <ti.ricardo.sousa@gmail.com>
2019-04-02 21:27:46 -0300

    Adicionando a dependência do Firebase-crash e a versão 1.28.0 do classpath do Fabric. Também foi criado uma activity própria pra fazer o teste de crash.

--- app/src/main/java/br/com/ricardo/firebasebasics/CloudMessaging.java
+++ app/src/main/java/br/com/ricardo/firebasebasics/CloudMessaging.java
@@ -1,19 +1,14 @@
 package br.com.ricardo.firebasebasics;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
-import com.crashlytics.android.Crashlytics;
 
 public class CloudMessaging extends AppCompatActivity {
 
 
     private TextView textCloudMessaging;
-    private Button buttonErro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +18,6 @@ public class CloudMessaging extends AppCompatActivity {
 
 
         textCloudMessaging = (TextView) findViewById(R.id.textNotification);
-        buttonErro = (Button) findViewById(R.id.button_erro);
 
 
         if(getIntent().getExtras() != null){
@@ -34,19 +28,6 @@ public class CloudMessaging extends AppCompatActivity {
                 textCloudMessaging.setText(v);
             }
         }
-
-        buttonErro.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent intent = new Intent(CloudMessaging.this, MyFirebaseMessagingService.class);
-                startActivity(intent);
-
-                Crashlytics.getInstance().crash();
-
-
-            }
-        });
 
 
     }
